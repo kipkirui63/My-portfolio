8a07d32b0da2e5107b73b8702f1a364b800a0513
kipkirui63 <enocksang8356@gmail.com>
2023-10-22 23:48:52 +0300

    added a text zone

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -11,3 +11,25 @@ import {
 import Loader from 'react-loaders'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './AboutContent.css'
+
+const AboutContent= () => {
+    return (
+      <>
+        <div className="container about-page">
+          <div className="text-zone">
+          
+            <p>
+            I'm a highly motivated individual ready to make a difference in the world of technology. 
+             I have a strong grasp of JavaScript and react and I am always looking for ways to continue to grow my skill set and stay up to date with the latest trends.
+             Driven by a desire to create engaging user experiences,
+             I'm dedicated to creating code and products that are reliable and bug-free
+            </p>
+            <p >
+              I'm quiet confident, naturally curious, and perpetually working on
+              improving my chops one design problem at a time.
+            </p>
+            <p>
+              If I need to define myself in one sentence that would be a willing to learn 
+              person,and tech-obsessed!!!
+            </p>
+          </div>
