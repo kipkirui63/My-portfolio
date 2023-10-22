b45a2490a5d15942320420bf90837716e074737f
kipkirui63 <enocksang8356@gmail.com>
2023-10-23 00:32:59 +0300

    added form for input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,3 +28,23 @@ function Form() {
           }
         );
     }
+    return (
+    
+        <div className='form'>
+          <form ref={refForm} onSubmit={sendEmail}>
+            <label>Your Name</label> 
+            <input type="text" name='text' required></input>
+            <label>Email</label>
+            <input type="email"  name='email' required></input>
+            <label>Subject</label>
+            <input type="text" name='text'  required></input>
+            <label>Message</label>
+            <textarea name="message" id=""  cols="30" rows="6" placeholder='Type in your message'/>
+    
+            <button type="submit" className='btn'>Submit</button>
+          </form>
+        </div>
+      )
+    }
+    
+    export default Form
