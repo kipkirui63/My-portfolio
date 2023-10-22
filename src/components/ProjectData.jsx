1a9d5e281bddde906238babf99f936c3ef3700ff
kipkirui63 <enocksang8356@gmail.com>
2023-10-23 00:52:45 +0300

    added links to done projects

---
+++ src/components/ProjectData.jsx
@@ -0,0 +1,27 @@
+import image1 from '../assets/project1.png'
+import image2 from '../assets/project2.jpg'
+import image3 from '../assets/project3.jpg'
+const ProjectCardData =[
+    {
+        imgsrc:image1,
+        title:"HireGrove",
+        text:'This is wine-app where a user registers and can be able to purchase the wine of their choice.',
+        view:"https://github.com/kipkirui63/wine-Cart",
+        source:"https://github.com/kipkirui63/wine-Cart"
+    },
+    {
+        imgsrc:image2,
+        title:"E-commerceHub",
+        text:'This project is a command-line inventory management system using Python, SQLAlchemy, and Click, with features for customers, products, and sales.',
+        view:"https://github.com/kipkirui63/E-commerce-Hub",
+        source:"https://github.com/kipkirui63/E-commerce-Hub"
+    },
+    {
+        imgsrc:image3,
+        title:"Superheroes",
+        text:'Superheroes is a web application offering an API to manage superheroes and their superpowers. The project comprises a Flask-based backend and React frontend, facilitating hero data management. It includes a robust setup process, data migration, and testing options. The application features well-structured models for heroes, powers, and their associations, enforcing data validation. Various routes are available for accessing and modifying hero and power details, making it a versatile tool for superhero management.',
+        view:"https://github.com/kipkirui63/Superheroes",
+        source:"https://github.com/kipkirui63/Superheroes"
+    }
+]
+export default ProjectCardData;
