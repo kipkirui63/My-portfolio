a22fce3488b961ff0a4de15f8ec614ce04b570fb
kipkirui63 <enocksang8356@gmail.com>
2023-10-23 00:22:38 +0300

    added prevent default

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,9 @@
 import React, { useRef } from 'react'
 import './Form.css'
 import emailjs from 'emailjs-com'
+
+function Form() {
+    const refForm = useRef();
+  
+    const sendEmail =(e)=>{
+       e.preventDefault()
